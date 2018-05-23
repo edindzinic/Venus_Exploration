fb1942af60ae93a7e4f8fa54d9b80c23e539dc44
martinusje <martijn264@gmail.com>
2018-05-23 08:05:51 +0200

    Complete the obstacle avoidance program

--- Final/search.ino
+++ Final/search.ino
@@ -1,14 +1,14 @@
 void search(){
   bool found = false;
   long previousMillis = 0;
-  int servoPos = 45;
+  int servoPos = 60;
   int servoDir = 0;
   driveForward(100);
   while(!found){
     // drive the robot until it has found the rock
-    if(millis()-previousMillis >= 15) {
-      if(servoPos == 45) servoDir = 0;
-      if(servoPos == 135) servoDir = 1;
+    if(millis()-previousMillis >= 5) {
+      if(servoPos == 60) servoDir = 0;
+      if(servoPos == 120) servoDir = 1;
       if(servoDir == 0) {
         servoPos += 1; 
       } else if(servoDir == 1) {
@@ -17,31 +17,34 @@ void search(){
       soundServo.write(servoPos);
       previousMillis = millis();
     }
-    if(detectClif() == 1) { //detect black right
+    if(detectCliff() == 1) { //detect black right
       stopMotors();
+      driveDistance(-50,100);      
       turnDegrees(-random(30, 90), 100); //turn left
       driveForward(100);
     }
-    if(detectClif() == 2) { //detect black left
+    if(detectCliff() == 2) { //detect black left
       stopMotors();
+      driveDistance(-50,100);      
       turnDegrees(random(30, 90), 100); //turn right
       driveForward(100);
     }
-    if(detectClif() == 4) { //detect black on both
+    if(detectCliff() == 4) { //detect black on both
       stopMotors();
       driveDistance(-100,100);
       turnDegrees(90, 100); //turn right
       driveForward(100);
     }
-    if(measureDist() < 20) { //object is detected within 20cm
+    if(measureDist() < 25) { //object is detected within 20cm
       stopMotors();
       if(servoPos < 90) { //detected left, turn right
-        turnDegrees(90-servoPos, 100); //turn right
+        turnDegrees(-random(30, 90), 100); //turn right
       } else {
-        turnDegrees(-(servoPos - 90), 100); //turn right
+        turnDegrees(random(30, 90), 100); //turn left
       }
-      driveForward();
+      driveForward(100);
     }
     //if(stoneDetected()) found = true;
   }
 }
+
